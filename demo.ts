cefdc8a5badd564d9d43567b49dc47cae8260690
Alessandro Warth <alexwarth@gmail.com>
2024-09-26 18:27:24 -0700

    get rid of old demos

--- demo.ts
+++ demo.ts
@@ -45,41 +45,6 @@ import Vec from './src/lib/vec';
 (window as any).Constraint = Constraint;
 (window as any).Handle = Handle;
 
-const pinImage = new Image();
-pinImage.src = 'pin.png';
-let pinImageLoaded = false;
-pinImage.onload = () => {
-  console.log('pin image loaded!');
-  pinImageLoaded = true;
-};
-
-const fingerOfGod = document.createElement('input') as any;
-fingerOfGod.setAttribute('type', 'checkbox');
-fingerOfGod.defaultChecked = false;
-fingerOfGod.style.position = 'absolute';
-fingerOfGod.style.top = '60px';
-fingerOfGod.style.right = '30px';
-// document.body.appendChild(fingerOfGod);
-
-const toggleDemoButton = document.createElement('button');
-toggleDemoButton.textContent = 'toggle demo';
-toggleDemoButton.style.position = 'absolute';
-toggleDemoButton.style.top = '30px';
-toggleDemoButton.style.right = '30px';
-toggleDemoButton.onclick = toggleDemo;
-document.body.appendChild(toggleDemoButton);
-
-// typecast weightSlider to `any` to get the typechecker to shut up!
-const weightSlider = document.createElement('input') as any;
-weightSlider.setAttribute('type', 'range');
-weightSlider.min = 0;
-weightSlider.max = 5;
-weightSlider.step = 0.01;
-weightSlider.style.position = 'absolute';
-weightSlider.style.top = '2px';
-weightSlider.style.right = '30px';
-document.body.appendChild(weightSlider);
-
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 initCanvas();
@@ -545,274 +510,52 @@ canvas.addEventListener('pointerup', (e) => {
 function drag(h: Handle, pos: Position) {
   const c = h.hasPin
     ? constraints.pin(h) // user moves the pin
-    : constraints.finger(fingerOfGod.checked, h); // add/update finger constraint
+    : constraints.finger(false, h); // add/update finger constraint
   c.position = pos;
 }
 
 function stopDragging(h: Handle) {
   if (!h.hasPin) {
-    constraints.finger(fingerOfGod.checked, h).remove();
+    constraints.finger(false, h).remove();
   }
   addImplicitConstraints(h);
 }
-
-function addWeight(h: Handle) {
-  const weight = constraints.weight(h, 2).weight;
-  weightSlider.value = weight.value;
-  weightSlider.oninput = () => weight.lock(weightSlider.value);
-}
-
-interface Demo {
-  init(): void;
-  render(): void;
-}
-
-let demo: Demo;
-
-const demo1 = {
-  init() {},
-
-  render() {
-    ctx.lineWidth = 2;
-
-    for (const c of Constraint.all) {
-      renderConstraint(c);
-    }
-
-    if (drawingLines) {
-      for (let idx = 1; idx < drawingLines.length; idx++) {
-        drawLine(drawingLines[idx - 1], drawingLines[idx]);
-      }
-      drawLine(drawingLines[drawingLines.length - 1], pointer);
-    }
-
-    for (const line of lines) {
-      renderLine(line);
-    }
-
-    if (drawingArc) {
-      drawArc(drawingArc.a, drawingArc.b, drawingArc.c);
-    }
-
-    for (const arc of arcs) {
-      renderArc(arc);
-    }
-
-    for (const h of Handle.all) {
-      renderHandle(h);
-    }
-  },
-};
-
-const demo2 = {
-  init() {
-    const handles = [
-      { x: 37, y: 44 },
-      { x: 99, y: 44 },
-      { x: 161, y: 45 },
-      { x: 222, y: 45 },
-      { x: 283, y: 47 },
-      { x: 37, y: 205 },
-      { x: 99, y: 147 },
-      { x: 160, y: 109 },
-      { x: 221, y: 86 },
-      { x: 282, y: 73 },
-      { x: 343, y: 72 },
-      { x: 649, y: 44 },
-      { x: 587, y: 44 },
-      { x: 525, y: 45 },
-      { x: 464, y: 45 },
-      { x: 403, y: 47 },
-      { x: 649, y: 205 },
-      { x: 587, y: 147 },
-      { x: 526, y: 109 },
-      { x: 465, y: 86 },
-      { x: 404, y: 73 },
-    ].map((pos) => Handle.create(pos));
-
-    const triangles = [
-      [0, 5, 6],
-      [0, 6, 1],
-      [1, 6, 7],
-      [1, 7, 2],
-      [2, 7, 8],
-      [2, 8, 3],
-      [3, 8, 9],
-      [3, 9, 4],
-      [4, 9, 10],
-      [11, 17, 16],
-      [11, 12, 17],
-      [12, 18, 17],
-      [12, 13, 18],
-      [13, 19, 18],
-      [13, 14, 19],
-      [14, 20, 19],
-      [14, 15, 20],
-      [15, 10, 20],
-    ].map((indices) => indices.map((idx) => handles[idx]));
-
-    for (const [a, b, c] of triangles) {
-      addLine(a, b);
-      addLine(b, c);
-      addLine(c, a);
-      constraints.polarVector(a, b).distance.lock();
-      constraints.polarVector(b, c).distance.lock();
-      constraints.polarVector(c, a).distance.lock();
-    }
-
-    constraints.pin(handles[0]);
-    constraints.pin(handles[5]);
-    constraints.pin(handles[11]);
-    constraints.pin(handles[16]);
-
-    const weightHandle = Handle.create({ x: 343, y: 150 });
-    addLine(handles[10], weightHandle);
-    constraints.polarVector(handles[10], weightHandle).distance.lock();
-    addWeight(weightHandle);
-  },
-
-  render() {
-    // left
-    drawRotated(pinImage, 0, 29, 42);
-    drawRotated(pinImage, -60, 30, 210);
-
-    // right
-    drawRotated(pinImage, 180, 658, 47);
-    drawRotated(pinImage, -120, 650, 212);
-
-    for (const c of Constraint.all) {
-      renderConstraint(c);
-    }
-
-    for (const line of lines) {
-      renderLine(line);
-    }
-
-    for (const arc of arcs) {
-      renderArc(arc);
-    }
-
-    for (const h of Handle.all) {
-      renderHandle(h);
-    }
-  },
-};
-
-interface Part {
-  a: Handle;
-  b: Handle;
-  c: Handle;
-  d: Handle;
-}
-
-const adjustLabelPosition = new Set<Constraint>();
-
-const demo3 = {
-  init() {
-    let lastPart: Part | null = null;
-    for (let idx = 0; idx < 6; idx++) {
-      const part = this.makePart(
-        lastPart ? lastPart.c : Handle.create({ x: 50, y: 50 }),
-        lastPart ? lastPart.d : Handle.create({ x: 50, y: 150 }),
-      );
-      if (!lastPart) {
-        constraints.pin(part.b);
-      }
-      if (idx === 1) {
-        const weightHandle = Handle.create({ x: part.d.x, y: part.d.y + 150 });
-        constraints.polarVector(part.d, weightHandle).distance.lock();
-        addLine(part.d, weightHandle);
-        const weight = constraints.weight(weightHandle, 2).weight;
-        weightSlider.value = weight.value;
-        weightSlider.oninput = () => weight.lock(weightSlider.value);
-      }
-      lastPart = part;
-    }
-    addLine(lastPart!.c, lastPart!.d);
-    constraints.polarVector(lastPart!.c, lastPart!.d).distance.lock();
-    constraints.pin(lastPart!.d);
-  },
-
-  makePart(a: Handle, b: Handle): Part {
-    const c = Handle.create({ x: a.x + 100, y: a.y });
-    const d = Handle.create({ x: b.x + 100, y: b.y });
-    constraints.polarVector(a, b).distance.lock();
-    constraints.polarVector(a, c).distance.lock();
-    constraints.polarVector(b, c).distance.lock();
-    constraints.polarVector(b, d).distance.lock();
-    constraints.polarVector(a, d).distance.lock();
-    addLine(a, b);
-    addLine(a, c);
-    addLine(b, c);
-    addLine(b, d);
-    addLine(a, d);
-    adjustLabelPosition.add(constraints.polarVector(a, d));
-    adjustLabelPosition.add(constraints.polarVector(b, c));
-    return { a, b, c, d };
-  },
-
-  render() {
-    drawRotated(pinImage, -90, 47, 157);
-    drawRotated(pinImage, -90, 647, 157);
-
-    for (const c of Constraint.all) {
-      renderConstraint(c);
-    }
-
-    for (const line of lines) {
-      renderLine(line);
-    }
-
-    for (const h of Handle.all) {
-      renderHandle(h);
-    }
-  },
-};
-
-const demos = [demo1, demo2, demo3];
-
-function toggleDemo() {
-  adjustLabelPosition.clear();
-  for (const constraint of Constraint.all) {
-    constraint.remove();
-  }
-  selection.clear();
-  for (const handle of Handle.all) {
-    handle.remove();
-  }
-  draggingHandle = null;
-  drawingLines = null;
-  while (lines.length > 0) {
-    lines.pop();
-  }
-  while (arcs.length > 0) {
-    arcs.pop();
-  }
-
-  const demoIdx = demos.indexOf(demo);
-  demo = demos[(demoIdx + 1) % demos.length];
-  demo.init();
-}
-
-toggleDemo();
 
 function render() {
   constraints.solve();
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  demo.render();
+
+  ctx.lineWidth = 2;
+
+  for (const c of Constraint.all) {
+    renderConstraint(c);
+  }
+
+  if (drawingLines) {
+    for (let idx = 1; idx < drawingLines.length; idx++) {
+      drawLine(drawingLines[idx - 1], drawingLines[idx]);
+    }
+    drawLine(drawingLines[drawingLines.length - 1], pointer);
+  }
+
+  for (const line of lines) {
+    renderLine(line);
+  }
+
+  if (drawingArc) {
+    drawArc(drawingArc.a, drawingArc.b, drawingArc.c);
+  }
+
+  for (const arc of arcs) {
+    renderArc(arc);
+  }
+
+  for (const h of Handle.all) {
+    renderHandle(h);
+  }
 
   requestAnimationFrame(render);
-}
-
-function drawRotated(image: HTMLImageElement, degrees: number, x: number, y: number) {
-  ctx.save();
-  ctx.globalAlpha = Math.random() * 0.2 + 0.7;
-  ctx.translate(x, y);
-  ctx.rotate((degrees * Math.PI) / 180);
-  ctx.translate(-image.width / 2, -image.height / 2);
-  ctx.drawImage(image, 0, 0);
-  ctx.restore();
 }
 
 requestAnimationFrame(render);
@@ -835,11 +578,7 @@ function renderConstraint(c: Constraint) {
       ctx.fillStyle = flickeryWhite();
       const fontSizeInPixels = 12;
       ctx.font = `${fontSizeInPixels}px Major Mono Display`;
-      const value =
-        demo === demo1
-          ? c.distance.value
-          : ((c.distance.value - Vec.dist(a, b)) / c.distance.value) * 10000;
-      let label = value.toFixed(0);
+      let label = c.distance.value.toFixed(0);
       if (label === '-0') {
         label = '0';
       }
@@ -847,19 +586,7 @@ function renderConstraint(c: Constraint) {
         label = ' ' + label;
       }
       const labelWidth = ctx.measureText(label).width;
-      if (adjustLabelPosition.has(c)) {
-        ctx.fillText(
-          label,
-          (a.x + 2 * b.x) / 3 - labelWidth / 2,
-          (a.y + 2 * b.y) / 3 + fontSizeInPixels / 2,
-        );
-      } else {
-        ctx.fillText(
-          label,
-          (a.x + b.x) / 2 - labelWidth / 2,
-          (a.y + b.y) / 2 + fontSizeInPixels / 2,
-        );
-      }
+      ctx.fillText(label, (a.x + b.x) / 2 - labelWidth / 2, (a.y + b.y) / 2 + fontSizeInPixels / 2);
     }
   } else if (c instanceof Weight) {
     ctx.strokeStyle = flickeryWhite();
