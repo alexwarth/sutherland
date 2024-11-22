152f36e3e161dcb0a75530b90cc01e768f2bf117
Alessandro Warth <alexwarth@gmail.com>
2024-11-21 22:35:29 -0800

    add rotateAround(p, c, angle) (untested)

--- src/helpers.ts
+++ src/helpers.ts
@@ -37,3 +37,18 @@ function pointDistToLineSegment2(p: Position, v: Position, w: Position) {
   const t = Math.max(0, Math.min(((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l, 1));
   return pointDist2(p, { x: v.x + t * (w.x - v.x), y: v.y + t * (w.y - v.y) });
 }
+
+export function rotateAround(p: Position, c: Position, angle: number): Position {
+  // Translate point to the origin
+  const tx = p.x - c.x;
+  const ty = p.y - c.y;
+
+  // Rotate
+  const sin = Math.sin(angle);
+  const cos = Math.cos(angle);
+  const rx = tx * cos - ty * sin;
+  const ry = tx * sin + ty * cos;
+
+  // Translate point back to its original position
+  return { x: rx + c.x, y: ry + c.y };
+}
