a6bc80dc20000e76fd7e66456fe07c07e1ddc783
Vanessa Freudenberg <vanessa@codefrau.net>
2025-03-13 20:53:18 -0700

    display: pseudo pen location

--- src/display.ts
+++ src/display.ts
@@ -1,6 +1,14 @@
-// import 'webgl-lint'; // for debugging
+import 'webgl-lint'; // for debugging
 import * as twgl from 'twgl.js';
 import * as dat from 'dat.gui';
+import * as wrapper from './wrapper';
+import * as NativeEvents from './NativeEvents';
+import config from './config';
+import { showHideConsole } from './console';
+
+config().usePredictedEvents = true;
+config().console = true;
+showHideConsole();
 
 // Simulate a CRT's glow by drawing into a phosphor texture,
 // thereby "depositing" photons into the phosphor, and then
@@ -82,14 +90,14 @@ void main() {
 }`;
 
 const uniforms = {
-    spotSize: 15,
+    spotSize: 10,
     screenScale: [512, 512],
     fadeAmount: 0.2,
 };
 
 const params = {
-    spots: 4000,
-    speed: 10,
+    demoSpots: 4000,
+    demoSpeed: 10,
     penTracker: true,
     fullscreen: false,
 }
@@ -114,26 +122,29 @@ function startup(canvas: HTMLCanvasElement) {
     resize();
 
     const gui = new dat.GUI();
-    gui.add(uniforms, 'spotSize', 1, 512);
+    gui.add(uniforms, 'spotSize', 1, 256);
     gui.add(uniforms, 'fadeAmount', 0, 1);
-    gui.add(params, 'spots', 1, MAX_SPOTS);
-    gui.add(params, 'speed', 0, 100);
-    gui.add(params, 'penTracker').onChange((on: boolean) => {
-        canvas.style.cursor = on ? 'none' : 'default';
-    });
+    gui.add(params, 'demoSpots', 1, MAX_SPOTS-348); // leave room for penTracker
+    gui.add(params, 'demoSpeed', 0, 100);
     gui.add(params, 'fullscreen').onChange((on: boolean) => {
         if (on) document.body.requestFullscreen();
         else document.exitFullscreen();
     });
 
     const penLoc = { x: 0, y: 0 };
-    canvas.onpointerdown = (e) => e.preventDefault();
-    canvas.onpointermove = (e) => {
+    function updatePen(x, y) {
         const b = canvas.getBoundingClientRect();
-        penLoc.x = (e.clientX - b.left - b.width/2) / b.width * 2 * uniforms.screenScale[0] | 0;
-        penLoc.y = (e.clientY - b.top - b.height/2) / b.height * -2 * uniforms.screenScale[1] | 0;
+        penLoc.x = (x - b.left - b.width/2) / b.width * 2 * uniforms.screenScale[0] | 0;
+        penLoc.y = (y - b.top - b.height/2) / b.height * -2 * uniforms.screenScale[1] | 0;
     };
-    canvas.focus();
+    canvas.onpointermove = canvas.onpointerdown = canvas.onpointerup = (e) => {
+        updatePen(e.clientX, e.clientY);
+    }
+    function processNativeEvents() {
+        for (const event of NativeEvents.getQueuedEvents()) {
+            if (event.type === 'pencil') updatePen(event.position.x, event.position.y);
+        }
+    }
 
     const fadeProg = twgl.createProgramInfo(gl, [VERT_SHADER, FADE_SHADER]);
     const fadeArrays: twgl.Arrays = {
@@ -156,19 +167,20 @@ function startup(canvas: HTMLCanvasElement) {
     let phase = 0;
     let prevTime = 0;
     function render(time: number) {
-        if (!spotsBuffers.attribs) throw new Error('No buffer attribs');
-        phase += (time - prevTime) * params.speed / 10000;
+        phase += (time - prevTime) * params.demoSpeed / 10000;
         prevTime = time;
+
+        processNativeEvents();
 
         // start add spots
         clearSpots();
-        if (params.penTracker) penTracker(penLoc.x, penLoc.y);
-        lissajous(400, 400, phase, 3, 4, params.spots);
+        lissajous(400, 400, phase, 3, 4, params.demoSpots);
+        penTracker(penLoc); // after all other spots
         // end add spots
 
         // update spots buffer
         twgl.setAttribInfoBufferFromArray(gl,
-            spotsBuffers.attribs.position,
+            spotsBuffers.attribs!.position,
             new Int16Array(displayTable.buffer, 0, numSpots*2));
 
         // render phosphor fade
@@ -192,19 +204,49 @@ function startup(canvas: HTMLCanvasElement) {
     requestAnimationFrame(render);
 }
 
-function penTracker(x: number, y: number) {
+let hadPseudoLoc = false;
+function penTracker({ x, y}) {
     // log pattern from fig 4.4 of Sketchpad thesis (pg. 58)
-    const count = 6;          // number of spots per arm
-    const start = 1.8;        // inner opening
-    const density = 1.3;      // density of spots
+    const COUNT = 6;          // number of spots per arm
+    const START = 2;          // inner opening
+    const DENSITY = 1.4;      // density of spots
+    const PSEUDO = 0.5;       // snap distance for pseudo pen location
+    const BRIGHT = 0.05;       // bright dot size for pseudo pen location
     const scale = Math.max(20, uniforms.spotSize);
-    for (let i = 0; i < count; i++) {
-        const r = Math.log(start + i) / density;
+    const pseudoRange = scale * PSEUDO;  // snap to spot this close
+    // find closest spot, make it the pseudo pen location
+    let pseudoX = x, pseudoY = y, dist = Infinity;
+    for (let i = 0; i < numSpots; i++) {
+        const dx = x - displayTable[2*i];
+        if (dx > pseudoRange || dx < -pseudoRange) continue;
+        const dy = y - displayTable[2*i+1];
+        if (dy > pseudoRange || dy < -pseudoRange) continue;
+        const d = Math.min(dx, dy);
+        if (d < dist) {
+            pseudoX = displayTable[2*i];
+            pseudoY = displayTable[2*i+1];
+            dist = d;
+        }
+    }
+    // haptic feedback
+    const hasPseudoLoc = dist < Infinity;
+    if (hasPseudoLoc !== hadPseudoLoc) {
+        if (hasPseudoLoc) {
+            wrapper.send('prepareHaptics');
+            wrapper.send('hapticImpact');
+        }
+        hadPseudoLoc = hasPseudoLoc;
+    }
+    // draw bright dot at pseudo pen location
+    const r = scale * BRIGHT;
+    for (const [ox, oy] of [[-2,-2], [-3,0], [-2,2], [0,-3], [0,0], [0,3], [2,-2], [3,0], [2,2]]) {
+        addSpot(pseudoX + ox * r, pseudoY + oy * r);
+    }
+    // draw arms
+    for (let i = 0; i < COUNT; i++) {
+        const r = Math.log(START + i) / DENSITY * scale;
         for (const [ox, oy] of [[-1,0], [1,0], [0,-1], [0,1]]) {
-            addSpot(
-                x + ox * scale * r,
-                y + oy * scale * r,
-            );
+            addSpot(x + ox * r, y + oy * r);
         }
     }
 }
