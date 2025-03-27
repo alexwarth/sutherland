805896fb1a832ac0f43f71bf51fcf4a0503a3c28
Vanessa Freudenberg <vanessa@codefrau.net>
2025-03-26 17:04:23 -0700

    better display for colorized spot indices

--- src/display.ts
+++ src/display.ts
@@ -22,6 +22,7 @@
 // [ ] use for Sketchpad drawing
 // [ ] use >8bit textures for higher dynamic range
 // [ ] avoid uploading 8 buffers for interlacing (indexed or instancing maybe?)
+// [ ] fix interlacing (up to 8 spots are not drawn at the end)
 
 import 'webgl-lint'; // for debugging
 import * as twgl from 'twgl.js';
@@ -187,33 +188,24 @@ in vec2 uv;
 out vec4 photons;
 
 // Simplex Noise Function
-vec2 mod289(vec2 x) {
-    return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-
-vec3 mod289(vec3 x) {
-    return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-
-vec3 permute(vec3 x) {
-    return mod289(((x*34.0)+1.0)*x);
-}
+vec2 mod289(vec2 x) { return x - floor(x / 289.0) * 289.0; }
+vec3 mod289(vec3 x) { return x - floor(x / 289.0) * 289.0; }
+vec3 permute(vec3 x) { return mod289((x * 34.0 + 1.0) * x); }
 
 float simplexNoise(vec2 v) {
     const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);
-    vec2 i  = floor(v + dot(v, C.yy) );
-    vec2 x0 = v -   i + dot(i, C.xx);
+    vec2 i = floor(v + dot(v, C.yy) );
+    vec2 x0 = v - i + dot(i, C.xx);
     vec2 i1;
     i1.x = step( x0.y, x0.x );
     i1.y = 1.0 - i1.x;
     vec4 x12 = x0.xyxy + C.xxzz;
     x12.xy -= i1;
     i = mod289(i);
-    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
-            + i.x + vec3(0.0, i1.x, 1.0 ));
+    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));
     vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
-    m = m*m ;
-    m = m*m ;
+    m *= m;
+    m *= m;
     vec3 x = 2.0 * fract(p * C.www) - 1.0;
     vec3 h = abs(x) - 0.5;
     vec3 ox = floor(x + 0.5);
@@ -227,7 +219,8 @@ float simplexNoise(vec2 v) {
 
 void main() {
     // noise to simulate phosphor roughness
-    float p = phosphorAmbient * mix(simplexNoise(uv * pixelRatio / phosphorGrain * 500.0) * 2.0, 1.0, phosphorSmoothness);
+    float noise = simplexNoise(uv * pixelRatio / phosphorGrain * 500.0);
+    float p = phosphorAmbient * mix(noise * 2.0, 1.0, phosphorSmoothness);
     // used with src * src.a + dst*(1-src.a)) blending to fade out
     photons = vec4(p, p, p, phosphorSpeed);
 }`;
@@ -278,7 +271,7 @@ void main() {
     uint idx = uint(xyIdIx.y & 65535);                     // 16 bit index
     if (idx < colorIdx) {
         float fraction = float(idx) / float(colorIdx);     // map to [0, 1)
-        v_color = hue(float(idx) / float(colorIdx));       // color by table index
+        v_color = hue(fraction * 0.7);                     // color by table index from red to purple
     } else {
         v_color = vec3(1.0);                               // default: white
     }
