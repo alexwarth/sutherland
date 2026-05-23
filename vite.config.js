96571607db58ef26cd3b2293ac0a8d07c06632df
Alessandro Warth <alexwarth@gmail.com>
2026-05-22 18:14:04 -0700

    fixed build script

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'sutherland',
-    },
-  },
+export default defineConfig(({ mode }) => ({
+  base: mode === 'deploy' ? '/projects/sutherland/' : '/',
+  build:
+    mode === 'deploy'
+      ? { outDir: 'sutherland' }
+      : {
+          lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'sutherland',
+          },
+        },
   server: {
     host: true,
-  }
-});
+  },
+}));
